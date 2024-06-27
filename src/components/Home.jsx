b9d904d79d153077887501c570e1140554c4ccbf
karanbadhwar <karanbadhwar07@gmail.com>
2024-06-27 15:55:22 -0400

    TopNav for main screen started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import SideNav from "../pages/SideNav";
+import TopNav from "../pages/TopNav";
 
 const Home = () => {
   document.title = "Date Night | HomePage";
   return (
     <>
       <SideNav />
-      <div className="w-[80%] h-full "></div>
+      <div className="w-[80%] h-full">
+        <TopNav />
+      </div>
     </>
   );
 };
