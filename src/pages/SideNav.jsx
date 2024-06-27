26a78ffe13887a025faeaa5b6b6b85e58adde691
karanbadhwar <karanbadhwar07@gmail.com>
2024-06-27 15:55:04 -0400

    Side Nav Compleetd for larger screens

--- src/pages/SideNav.jsx
+++ src/pages/SideNav.jsx
@@ -2,36 +2,53 @@ import { Link } from "react-router-dom";
 
 function SideNav() {
   return (
-    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-3">
+    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
       <h1 className="text-white font-bold">
-        <i className="text-[#6556CD] ri-tv-fill text-2xl mr-3"></i>
-        <span className="text-xl sm:text-2xl hidden lg:inline-block">
+        <i className="text-[#6556CD] ri-tv-fill mr-2 text-2xl "></i>
+        <span className="text-xl  ">
           Date <span className="text-[#6556CD]">Night</span>
         </span>
-        <span className="text-2xl lg:hidden">
-          d<span className="text-[#6556CD]">N</span>
-        </span>
+        {/* <span className="text-xl  mr-2">
+          d<span className="text-[#6556CD] ml-1">N</span>
+        </span> */}
       </h1>
 
-      <nav className="flex flex-col text-zinc-400 text-md sm:text-xl sm:gap-3 gap-2">
-        <h1 className="text-white font-semibold sm:text-xl text-md mt-10 mb-5">
+      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
+        <h1 className="text-white font-semibold text-xl mt-10 mb-2">
           New Feeds
         </h1>
 
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg sm:p-5 p-1 duration-300">
-          <i className="ri-fire-fill"></i> Trending
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-fire-fill mr-2"></i> Trending
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg sm:p-5 p-1 duration-300">
-          <i className="ri-bard-fill"></i> Popular
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-bard-fill mr-2"></i> Popular
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg sm:p-5 p-1 duration-300">
-          Movies
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-movie-2-fill mr-2"></i> Movies
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg sm:p-5 p-1 duration-300">
-          Tv Shows
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-tv-2-fill mr-2"></i> Tv Shows
         </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg sm:p-5 p-1 duration-300">
-          People
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-user-search-fill mr-2"></i> People
+        </Link>
+      </nav>
+
+      <hr className="border-none h-[1px] bg-zinc-400 mt-2" />
+
+      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
+        <h1 className="text-white font-semibold sm:text-xl text-md mt-10 mb-5">
+          Website information
+        </h1>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-information-fill mr-2"></i> About
+          <span className="text-[#6556CD] font-semibold ml-1 hover:text-white">
+            dN
+          </span>
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg  py-3 px-5 duration-300">
+          <i className="ri-phone-fill mr-2"></i> Contact Us
         </Link>
       </nav>
     </div>
